994fcc67112ec72111e560c03f8ab533e16e374e
Joseph Hunkeler <jhunkeler@gmail.com>
2013-06-03 14:16:23 -0400

    Important bugfix: no new lines in generated scripts

--- ipsutils/tasks.py
+++ ipsutils/tasks.py
@@ -262,6 +262,7 @@ class Script(task.Task):
             t = string.Template(string.join(line))
             line = t.safe_substitute(self.cls.env_pkg)
             fp_tempfile.writelines(line)
+            fp_tempfile.writelines('\n')
             if self.cls.options.verbose:
                 print(">>> {0:s}".format(line))
         fp_tempfile.flush()
